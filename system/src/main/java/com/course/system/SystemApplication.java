a5163f8b3263fffb430023ff051fe38842e30705
ooxx <simunian@email>
2020-11-24 14:35:39 +0800

    ,mm

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -5,9 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.course")
 @MapperScan(basePackages = "com.course.server.mapper")//扫描 mybatis mapper 包路径
 public class SystemApplication {
 
